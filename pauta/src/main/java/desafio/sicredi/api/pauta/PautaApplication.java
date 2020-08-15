bae8fdf964a470bc80924f8fc5575239b86b7ad0
William <wmp320@gmail.com>
2020-08-15 20:00:51 -0300

    Full commit projeto
    Commitando todos os arquivos do projeto de demo, não necessariamente alguns deles devam pertencer aqui.
    
    Verificar em outra ocasião quais são realmente necessários e que não deveriam ter sido trazidos caso o .gitignore não esteja funcionando 100%.
    
    Mas para quem for abrir o projeto usando a mesma IDE (STS) isto deve funcionar bem.

---
+++ pauta/src/main/java/desafio/sicredi/api/pauta/PautaApplication.java
@@ -0,0 +1,13 @@
+package desafio.sicredi.api.pauta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PautaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PautaApplication.class, args);
+	}
+
+}
